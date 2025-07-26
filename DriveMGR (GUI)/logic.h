c376d5be4709e3d6a4206e03cb1d843daffac16d
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2025-07-26 08:03:54 +0200

    Add files via upload

---
+++ DriveMGR (GUI)/logic.h
@@ -0,0 +1,15 @@
+#ifndef logic_h
+#define logic_h
+
+
+#include <string>
+#include <vector>
+
+void lostDrives(std::vector<std::string>& drives);
+void advancedListDrives();
+void formatDrive();
+void envryptDecryptDrive();
+void resizeDrive();
+int checkDriveHealth();
+
+#endif

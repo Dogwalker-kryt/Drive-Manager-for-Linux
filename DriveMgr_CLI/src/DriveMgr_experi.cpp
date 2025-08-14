e70d7f704548bc037a7222401fc637a9eb7ee845
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2025-08-14 20:41:51 +0200

    Update DriveMgr_experi.cpp

--- DriveMgr_CLI/src/DriveMgr_experi.cpp
+++ DriveMgr_CLI/src/DriveMgr_experi.cpp
@@ -1,4 +1,21 @@
-// ! Warning this version has bugs and so or is not finisehd or tested 
+/*
+ * DriveMgr - Linux Drive Management Utility
+ * Copyright (C) 2025 Dogwalker-kryt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// ! Warning this version has bugs and so or is not finisehd 
 // The correct version is now in the folder DriveMgr with no bugs and such
 // Curretn version of this code is in the Info() function below
 // v0.8.88-01
